862c598d42f77de2822357cf20b6ddd3e0c147ff
Li <1214046231@qq.com>
2023-03-23 16:34:32 +0800

    Update useIntersectionObserver.ts

--- useIntersectionObserver.ts
+++ useIntersectionObserver.ts
@@ -5,32 +5,47 @@ import { useState, useEffect } from "react";
 /**
  *
  * @param { ref } reference
+ * @param { IntersectionObserverInit } intersectionOptions
  * @returns { boolean } Return true if the element is displayed in the current window
- * @scenseA Long and multi module
- * @scenseB Lazy load module
- * @scenseC Todo...
  */
 export const useIntersectionObserver = <T extends HTMLElement>(
-  reference: RefObject<T>
+  reference: RefObject<T>,
+  intersectionOptions?: IntersectionObserverInit
 ): boolean => {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false)
+
+  const observer = useRef<IntersectionObserver | null>(null)
 
   useEffect(() => {
-    const observer = new IntersectionObserver((entries, observer) => {
-      if (entries[0].isIntersecting) {
-        setIsVisible(true);
-        observer.unobserve(entries[0].target);
-        observer.disconnect();
+    ;(async () => {
+      // intersection-observer polyfill
+      // need? 
+      if (typeof window.IntersectionObserver === 'undefined') {
+        await import('intersection-observer')
       }
-    });
 
-    if (reference.current) {
-      observer.observe(reference.current);
-    }
+      const options: IntersectionObserverInit = {
+        // detect 30% percent in page area
+        threshold: 0.3,
+        ...intersectionOptions,
+      }
+
+      observer.current = new IntersectionObserver((entries, observer) => {
+        if (entries[0].isIntersecting) {
+          setIsVisible(true)
+          observer.unobserve(entries[0].target)
+          observer.disconnect()
+        }
+      }, options)
+
+      if (reference.current) {
+        observer.current.observe(reference.current)
+      }
+    })()
 
     // If unmounting, then disconnect
-    return () => observer.disconnect();
-  }, [reference]);
+    return () => observer.current?.disconnect()
+  }, [reference])
 
-  return isVisible;
-};
+  return isVisible
+}
